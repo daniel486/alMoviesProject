050a69e10d00b3007e206d0f1fbf1e855e92526a
daniel486 <daniel.felipe.garcia@correounivalle.edu.co>
2018-07-05 01:41:09 -0500

    Revert "Detail tv shows"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,63 +1,16 @@
-//Angular imports
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, Pipe } from '@angular/core';
-import { FormsModule }   from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
 
-//Material imports
-import {FlexLayoutModule} from '@angular/flex-layout';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-
-//Custom components imports
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { ListMoviesComponent } from './components/list-movies/list-movies.component';
-import { DetailMovieComponent } from './components/detail-movie/detail-movie.component';
-import { ListActorsComponent } from './components/list-actors/list-actors.component';
-import { DetailActorComponent } from './components/detail-actor/detail-actor.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { ContentComponent } from './components/content/content.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { SafePipe } from './pipes/safe.pipe';
-import { RoundPipe } from './pipes/round.pipe';
-import { ListTvComponent } from './components/list-tv/list-tv.component';
-import { DetailTvComponent } from './components/detail-tv/detail-tv.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ListMoviesComponent,
-    DetailMovieComponent,
-    ListActorsComponent,
-    DetailActorComponent,
-    NavbarComponent,
-    ContentComponent,
-    FooterComponent,
-    SafePipe,
-    RoundPipe,
-    ListTvComponent,
-    DetailTvComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    FlexLayoutModule,
-    MatButtonModule,
-    MatCardModule,
-    MatTabsModule,
-    MatToolbarModule,
-    MatIconModule
+    BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
-  exports: [ListMoviesComponent, DetailMovieComponent, ListActorsComponent, DetailActorComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
