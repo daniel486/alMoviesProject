938dd555897d057bfb5102af0f5c5a5be239ade8
daniel <daniel.felipe.garcia@correounivalle.edu.co>
2018-07-05 17:43:31 -0500

    Change the favIcon and stablish the logo for the application

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { SearchDescriptor } from '../types/search.type';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
 
-  constructor() { }
+  private url = "https://api.themoviedb.org/3/search/multi";
+  private apiKey = "?api_key=14383e7a1e2a63bc1e67c0052614384f";
+  private query = "&query=";
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  /**
+   * This function provide the multiple search from TMDB API to search
+   * movies, actors and tv series based on a query.
+   * @param queryInput This query is to get the information from TMDB API
+   */
+  getMultipleSearch(queryInput: string){
+    return this.http.get(this.url + this.apiKey + this.query + queryInput)
+    .pipe(
+      map((data)=>{
+        return SearchDescriptor.import(data);
+      })
+    );
+  }
 }
