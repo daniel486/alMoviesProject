4e7c5ac6e71933f6a4b9e5016ffb199a28d87317
daniel <daniel.felipe.garcia@correounivalle.edu.co>
2018-07-05 20:56:16 -0500

    Search function and little fixes to footer

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchDescriptor } from '../../types/search.type';
+import { SearchService } from '../../services/search.service';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  public search: SearchDescriptor = new SearchDescriptor();
+  public query: string = "";
+  public serieType: string = "tv";
+  public movieType: string = "movie";
+  public peopleType: string = "person";
+  
+
+  constructor(
+    private searchService: SearchService
+  ) { }
 
   ngOnInit() {
+  }
+
+  searchMulti(){
+    if(this.query){
+      this.searchService.getMultipleSearch(this.query).subscribe(
+        (data: any) => {
+          this.search = data;
+        }
+      );
+    }else{
+      this.search = new SearchDescriptor;
+    }
+  }
+  
+  /**
+  * This function clean the query input
+  **/
+  cleanInput() {
+    this.query = "";
+    this.search = new SearchDescriptor
   }
 
 }
