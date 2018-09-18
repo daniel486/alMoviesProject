bb0caec6986be9da4c56e5581ff29ff2855f59d5
Daniel <daniel.felipe.garcia@correounivalle.edu.co>
2018-09-18 15:22:02 -0500

    Test for the Pipe round / Service Pack 18-09-2018

--- src/app/pipes/round.pipe.spec.ts
+++ src/app/pipes/round.pipe.spec.ts
@@ -1,8 +1,16 @@
 import { RoundPipe } from './round.pipe';
 
 describe('RoundPipe', () => {
-  it('create an instance', () => {
-    const pipe = new RoundPipe();
-    expect(pipe).toBeTruthy();
+  let pipe: RoundPipe;
+
+  beforeEach(() => {
+    pipe = new RoundPipe();
+  });
+
+  it('should round the decimal number', () => {
+    let nextResult = pipe.transform(45.675);
+    expect(nextResult).toEqual(46);
+    let sameResult = pipe.transform(37.42);
+    expect(sameResult).toEqual(37);
   });
 });
