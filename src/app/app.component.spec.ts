edb53f4b7d550ae807f952fa5ce573e19c78aab3
Daniel <daniel.felipe.garcia@correounivalle.edu.co>
2018-09-19 12:12:11 -0500

    Test for the AppComponent / Service Pack 19-09-2018

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,55 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, ComponentFixture, async } from '@angular/core/testing';
+import { ContentComponent } from './components/content/content.component';
 import { AppComponent } from './app.component';
+import { ListMoviesComponent } from './components/list-movies/list-movies.component';
+import { DetailMovieComponent } from './components/detail-movie/detail-movie.component';
+import { ListActorsComponent } from './components/list-actors/list-actors.component';
+import { DetailActorComponent } from './components/detail-actor/detail-actor.component';
+import { ListTvComponent } from './components/list-tv/list-tv.component';
+import { DetailTvComponent } from './components/detail-tv/detail-tv.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { SafePipe } from './pipes/safe.pipe';
+import { RoundPipe } from './pipes/round.pipe';
+
+
 describe('AppComponent', () => {
+
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
-      ],
+        AppComponent,
+        ListMoviesComponent,
+        DetailMovieComponent,
+        ListActorsComponent,
+        DetailActorComponent,
+        ListTvComponent,
+        DetailTvComponent,
+        ContentComponent,
+        FooterComponent,
+        SafePipe,
+        RoundPipe], //Here we put all the components that use our component. 
+      schemas: [ NO_ERRORS_SCHEMA ]
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to alMoviesProject!');
-  }));
+
+  //Arrange
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+  });
+
+
+  fit('should create the app component', () => {
+    console.log(component);
+    expect(component).toBeDefined();
+  });
+
+  fit(`should have as title 'app'`, () => {
+    expect(component.title).toEqual('app');
+  });
+
 });
